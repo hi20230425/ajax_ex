0eade01f22ece06c6220299b9cf53076d1ec1e2b
Jangwoo Park <p.jangwoo@gmail.com>
2023-08-11 11:52:02 +0900

    8월 11일 12시

---
+++ src/test/java/com/mysite/ajax_ex/AjaxExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.ajax_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjaxExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
