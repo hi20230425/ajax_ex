67f5b2f3f4ef3488a858c45b34f22d896873f7e9
Jangwoo Park <p.jangwoo@gmail.com>
2023-08-11 18:06:06 +0900

    8월 11일 - 저녁

--- src/main/java/com/mysite/ajax_ex/controller/AjaxController.java
+++ src/main/java/com/mysite/ajax_ex/controller/AjaxController.java
@@ -1,9 +1,15 @@
 package com.mysite.ajax_ex.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -74,7 +80,10 @@ public class AjaxController {
 	// client Ajax 에서 변수값을 넘김  @RequestParam 어노테이션으로 값을 받기 
 	@PostMapping("/ajax/ex04")	// Ajax를 통한 비동기 통신 
 	public @ResponseBody String Ex04Ajax(
-			@RequestParam String param1, @RequestParam String param2
+			//@RequestParam 
+			String param1, 
+			//@RequestParam 
+			String param2
 			) {
 		System.out.println("Ajax Controller : ext04 요청 성공 ");
 		System.out.println("param1 : " + param1);
@@ -94,7 +103,8 @@ public class AjaxController {
 	// client Ajax 에서 변수값을 넘김  @RequestParam 어노테이션으로 값을 받기 
 	@GetMapping("/ajax/ex05")	// Ajax를 통한 비동기 통신 
 	public @ResponseBody String Ex05Ajax(
-			@ModelAttribute AjaxDTO ajaxDTO
+		//	@ModelAttribute 
+			AjaxDTO ajaxDTO
 			) {
 		System.out.println("Ajax Controller : ext05 요청 성공 ");
 		System.out.println("param1 : " + ajaxDTO.getParam1());
@@ -104,5 +114,143 @@ public class AjaxController {
 		return "ex05 요청 처리 완료됨"; 	// @ResponseBody : 리턴 타입 앞에 할당, JSON 포멧으로 변환후 전송 	
 	}
 	
+	
+	// @RequestParam : 변수 1개 과 @ModelAttribute : dto 에 한꺼번에 주입 는 생략 가능 
+	
+	@GetMapping ("/ex06")		//http://localhost:9797/ex06
+	public String Ex06View() {
+		
+		return "ajax-ex-06"; 
+	}
+	
+	@PostMapping("/ajax/ex06")	// Ajax를 통한 비동기 통신 
+	public @ResponseBody AjaxDTO Ex06Ajax(
+		//	@ModelAttribute 
+			AjaxDTO ajaxDTO
+			) {
+		System.out.println("Ajax Controller : ext06 요청 성공 ");
+		System.out.println("param1 : " + ajaxDTO.getParam1());
+		System.out.println("param2 : " + ajaxDTO.getParam2());
+		System.out.println("ajxDTO : " + ajaxDTO);
+		
+		
+		return ajaxDTO; 	// @ResponseBody : 리턴 타입 앞에 할당, JSON 포멧으로 변환후 전송 	
+	}
+	
+	@GetMapping ("/ex07")		//http://localhost:9797/ex06
+	public String Ex07View() {
+		
+		return "ajax-ex-07"; 
+	}
+	
+	
+	
+	// @RequestBody : client (JSON) ===> JSON을 DTO (자바의객체) 로 주입 
+	@PostMapping("/ajax/ex07")	// Ajax를 통한 비동기 통신 
+	public @ResponseBody AjaxDTO Ex07Ajax(
+			@RequestBody 
+			AjaxDTO ajaxDTO
+			) {
+		System.out.println("Ajax Controller : ext07 요청 성공 ");
+		System.out.println("param1 : " + ajaxDTO.getParam1());
+		System.out.println("param2 : " + ajaxDTO.getParam2());
+		System.out.println("ajxDTO : " + ajaxDTO);
+		
+		
+		return ajaxDTO; 	// @ResponseBody : 리턴 타입 앞에 할당, JSON 포멧으로 변환후 전송 	
+	}
+	
+	@GetMapping ("/ex08")		//http://localhost:9797/ex06
+	public String Ex08View() {
+		
+		return "ajax-ex-08"; 
+	}
+	
+	@PostMapping("/ajax/ex08")	// Ajax를 통한 비동기 통신 
+	public @ResponseBody List<AjaxDTO> Ex08Ajax(
+			@RequestBody 
+			AjaxDTO ajaxDTO
+			) {
+		System.out.println("Ajax Controller : ext08 요청 성공 ");
+		System.out.println("param1 : " + ajaxDTO.getParam1());
+		System.out.println("param2 : " + ajaxDTO.getParam2());
+		System.out.println("ajxDTO : " + ajaxDTO);
+		
+		List<AjaxDTO> dtoList = new ArrayList(); 
+		
+		dtoList.add(ajaxDTO);    // client 의 넘어오는 dto를 그대로 List 에 추가 
+		
+		AjaxDTO ajaxDTO1 = new AjaxDTO (); 
+		ajaxDTO1.setParam1("홍길동"); 
+		ajaxDTO1.setParam2("남자"); 
+		
+		dtoList.add(ajaxDTO1); 
+		
+		AjaxDTO ajaxDTO2 = new AjaxDTO (); 
+		ajaxDTO2.setParam1("신사임당"); 
+		ajaxDTO2.setParam2("여자"); 
+		
+		dtoList.add(ajaxDTO2);
+		
+		return dtoList; 	// @ResponseBody : 리턴 타입 앞에 할당, JSON 포멧으로 변환후 전송 	
+	}
+	
+	@GetMapping ("/ex09")		//http://localhost:9797/ex06
+	public String Ex09View() {
+		
+		return "ajax-ex-09"; 
+	}
+	
+	
+	// ResponseEntity : JSON 타입으로 <객체, List> , <http 상태코드>
+	
+	@PostMapping("/ajax/ex09")	// Ajax를 통한 비동기 통신 
+	public  ResponseEntity Ex09Ajax(
+			@RequestBody 
+			AjaxDTO ajaxDTO
+			) {
+		System.out.println("Ajax Controller : ext07 요청 성공 ");
+		System.out.println("param1 : " + ajaxDTO.getParam1());
+		System.out.println("param2 : " + ajaxDTO.getParam2());
+		System.out.println("ajxDTO : " + ajaxDTO);
+		
+		
+		return new ResponseEntity<>(ajaxDTO, HttpStatus.OK); 	
+	}
+	
+	@GetMapping ("/ex10")		//http://localhost:9797/ex06
+	public String Ex10View() {
+		
+		return "ajax-ex-10"; 
+	}
+	
+	@PostMapping("/ajax/ex10")	// Ajax를 통한 비동기 통신 
+	public ResponseEntity Ex10Ajax(
+			@RequestBody 
+			AjaxDTO ajaxDTO
+			) {
+		System.out.println("Ajax Controller : ext08 요청 성공 ");
+		System.out.println("param1 : " + ajaxDTO.getParam1());
+		System.out.println("param2 : " + ajaxDTO.getParam2());
+		System.out.println("ajxDTO : " + ajaxDTO);
+		
+		List<AjaxDTO> dtoList = new ArrayList(); 
+		
+		dtoList.add(ajaxDTO);    // client 의 넘어오는 dto를 그대로 List 에 추가 
+		
+		AjaxDTO ajaxDTO1 = new AjaxDTO (); 
+		ajaxDTO1.setParam1("홍길동"); 
+		ajaxDTO1.setParam2("남자"); 
+		
+		dtoList.add(ajaxDTO1); 
+		
+		AjaxDTO ajaxDTO2 = new AjaxDTO (); 
+		ajaxDTO2.setParam1("신사임당"); 
+		ajaxDTO2.setParam2("여자"); 
+		
+		dtoList.add(ajaxDTO2);
+		
+		return new ResponseEntity<>(dtoList, HttpStatus.OK); 	// @ResponseBody : 리턴 타입 앞에 할당, JSON 포멧으로 변환후 전송 	
+	}
 
 }
--- src/main/java/com/mysite/ajax_ex/dto/AjaxDTO.java
+++ src/main/java/com/mysite/ajax_ex/dto/AjaxDTO.java
@@ -1,10 +1,13 @@
 package com.mysite.ajax_ex.dto;
 
+
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class AjaxDTO {
 	
 	private String param1; 
